024922a107074ebc578908baee5d227b484e2ae0
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-11-30 14:18:49 -0500

    passes all tests

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,28 @@
-package zipcode;
+package src.main.zipcode;
+import org.junit.rules.ExpectedException;
+import org.junit.Test;
 
 public class Hamming {
+    private String s;
+    private String s1;
+
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int hammingDistance = 0;
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c != s1.toCharArray()[count]) {
+                hammingDistance++;
+            }
+            count++;
+        }
+        return hammingDistance;
     }
 }
